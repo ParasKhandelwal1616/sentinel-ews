2cd18ae0767d4950a65f96c373b9b81b7e04fd9c
Paras Khandelwal <parasprince161616@gmail.com>
2026-02-18 17:48:46 +0530

    added backend and index.ts setup

---
+++ server/src/index.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import * as dotenv from "dotenv";
+import connectDB from "./config/database.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Connect to Database
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  console.log("Welcome to Sentinel API");
+  res.json({ message: "Welcome to Sentinel EWS API" }); // browser में दिखने के लिए
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is connected on port ${PORT}`);
+});
